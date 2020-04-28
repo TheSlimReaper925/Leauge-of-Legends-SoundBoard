c72f3e3f68c000e8b9985c99c4c7931be44737ea
theexplosionmage <otar.phrodiashvili.1@btu.edu.ge>
2020-04-29 01:15:41 +0400

    Updated page_start.py + background

--- page_start.py
+++ page_start.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from PIL import Image, ImageTk
 
 
 import page_start_support
@@ -8,7 +9,9 @@ def vp_start_gui():
     '''Starting point when module is the main routine.'''
     global val, w, root
     root = tk.Tk()
-    top = Toplevel1 (root)
+    img = Image.open("materials/pic_page_start_background.jpg")
+    photo = ImageTk.PhotoImage(img)
+    top = Toplevel1 (photo, root)
     page_start_support.init(root, top)
     root.mainloop()
 
@@ -30,59 +33,52 @@ def destroy_Toplevel1():
     w = None
 
 class Toplevel1:
-    def __init__(self, top=None):
+
+    def __init__(self, photo, top=None):
         '''This class configures and populates the toplevel window.
            top is the toplevel containing window.'''
-        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9' # X11 color: 'gray85'
-        _ana1color = '#d9d9d9' # X11 color: 'gray85'
-        _ana2color = '#ececec' # Closest X11 color: 'gray92'
+
         self.style = ttk.Style()
         self.style.theme_use('winnative')
-        self.style.configure('.',background=_bgcolor)
-        self.style.configure('.',foreground=_fgcolor)
-        self.style.configure('.',font="TkDefaultFont")
-        self.style.map('.',background=
-            [('selected', _compcolor), ('active',_ana2color)])
 
         top.geometry("800x580+652+102")
-        top.minsize(120, 1)
-        top.maxsize(1924, 1061)
-        top.resizable(1, 1)
-        top.title("New Toplevel")
-        top.configure(background="#d9d9d9")
+        top.resizable(0, 0)
+        top.title("League Of Legends Sound Board")
 
-        self.TLabelframe1 = ttk.Labelframe(top)
-        self.TLabelframe1.place(relx=0.313, rely=0.121, relheight=0.69
-                , relwidth=0.375)
-        self.TLabelframe1.configure(relief='raised')
-        self.TLabelframe1.configure(labelanchor="n")
-        self.TLabelframe1.configure(relief="raised")
-        self.TLabelframe1.configure(cursor="fleur")
 
-        self.TButton1 = ttk.Button(self.TLabelframe1)
-        self.TButton1.place(relx=0.167, rely=0.225, height=50, width=200
-                , bordermode='ignore')
+        self.Label = ttk.Label(top, image=photo)
+        self.Label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.TButton1 = ttk.Button(top)
+        self.TButton1.place(relx=0.5, rely=0.3, anchor=tk.CENTER, height=50, width=200, bordermode='ignore')
         self.TButton1.configure(command=page_start_support.toChampionsPage)
         self.TButton1.configure(takefocus="")
         self.TButton1.configure(text='''Champion List''')
-        self.TButton1.configure(cursor="fleur")
 
-        self.TButton1_1 = ttk.Button(self.TLabelframe1)
-        self.TButton1_1.place(relx=0.167, rely=0.375, height=50, width=200
+
+
+
+        self.TButton1_1 = ttk.Button(top)
+        self.TButton1_1.place(relx=0.5, rely=0.4, anchor=tk.CENTER, height=50, width=200
                 , bordermode='ignore')
         self.TButton1_1.configure(command=page_start_support.toGamePage)
         self.TButton1_1.configure(takefocus="")
         self.TButton1_1.configure(text='''Guessing Game''')
-        self.TButton1_1.configure(cursor="fleur")
 
-        self.TButton1_2 = ttk.Button(self.TLabelframe1)
-        self.TButton1_2.place(relx=0.167, rely=0.525, height=50, width=200
+
+
+
+
+
+        self.TButton1_2 = ttk.Button(top)
+        self.TButton1_2.place(relx=0.5, rely=0.5, anchor=tk.CENTER, height=50, width=200
                 , bordermode='ignore')
         self.TButton1_2.configure(command=page_start_support.toSupportLink)
         self.TButton1_2.configure(takefocus="")
         self.TButton1_2.configure(text='''Support Us''')
+
+
+
 
 if __name__ == '__main__':
     vp_start_gui()
--- test.py
+++ test.py
@@ -7,13 +7,12 @@ win.title("test app")
 win.geometry("800x580")
 win.resizable(False, False)
 
-
-# champions = ("Neeko", "other")
+champions = ("Neeko", "other")
 
 neeko_quotes = ("mimic jinx.ogg", "Neeko was out.ogg", "Neeko_063.ogg", "Neeko_Ban.ogg",
                 "Neeko_select.ogg", "not being neeko.ogg", "not neeko.ogg")
 
-# champions_selected = (neeko_quotes)
+champions_selected = (neeko_quotes)
 
 mixer.init()
 
@@ -23,31 +22,20 @@ def play_quote(quote):
     mixer.music.play()
 
 
-z = 1
-x = 0
-for i in neeko_quotes:
-    t.Button(win, command=lambda y=i: play_quote(y), text=i[0:-4]).grid(row=z, column=x)
-    x += 1
-    if x == 7:
+def get_btn(event):
+    if champ_select.current() == 0:
+        z = 1
         x = 0
-        z += 1
+        for i in neeko_quotes:
+            t.Button(win, command=lambda y=i: play_quote(y), text=i[0:-4]).grid(row=z, column=x)
+            x += 1
+            if x == 7:
+                x = 0
+                z += 1
 
 
-
-#
-# champ_select = t.Combobox(win, values=champions)
-# champ_select.grid(row=0)
-# champ_select.bind("<<ComboboxSelected>>", lambda: get_btns(champions_selected[champ_select.current()]))
-# btn2 = t.Button(win, command=lambda: play_quote("Neeko_Select.ogg"), text="Neeko select")
-# btn2.grid(row=0, column=1)
-#
-# btn3 = t.Button(win, command=lambda: play_quote("Neeko_063.ogg"), text="Neeko on Nidalee")
-# btn3.grid(row=0, column=2)
-#
-# btn4 = t.Button(win, command=lambda: play_quote("not neeko.ogg"), text="Not Neeko")
-# btn4.grid(row=0, column=3)
-#
-# btn4 = t.Button(win, command=lambda: play_quote("not being neeko.ogg"), text="Not being Neeko")
-# btn4.grid(row=0, column=4)
+champ_select = t.Combobox(win, values=champions)
+champ_select.grid(row=0)
+champ_select.bind("<<ComboboxSelected>>", get_btn)
 
 win.mainloop()
