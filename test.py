7d4e089341a16cb3ded68d763092ee7014b02655
theslimreaper925 <lasha.nuroshvili.1@btu.edu.ge>
2020-04-28 12:22:46 +0400

    improved code a little bit

--- test.py
+++ test.py
@@ -7,6 +7,14 @@ win.title("test app")
 win.geometry("800x580")
 win.resizable(False, False)
 
+
+# champions = ("Neeko", "other")
+
+neeko_quotes = ("mimic jinx.ogg", "Neeko was out.ogg", "Neeko_063.ogg", "Neeko_Ban.ogg",
+                "Neeko_select.ogg", "not being neeko.ogg", "not neeko.ogg")
+
+# champions_selected = (neeko_quotes)
+
 pygame.mixer.init()
 
 
@@ -15,19 +23,29 @@ def play_quote(quote):
     pygame.mixer.music.play()
 
 
-btn1 = t.Button(win, command=lambda: play_quote("Neeko_Ban.ogg"), text="Neeko Ban")
-btn1.grid(row=0, column=0)
+z = 1
+x = 0
+for i in neeko_quotes:
+    t.Button(win, command=lambda y=i: play_quote(y), text=i[0:-4]).grid(row=z, column=x)
+    x += 1
+    if x == 7:
+        x = 0
+        z += 1
 
-btn2 = t.Button(win, command=lambda: play_quote("Neeko_Select.ogg"), text="Neeko select")
-btn2.grid(row=0, column=1)
-
-btn3 = t.Button(win, command=lambda: play_quote("Neeko_063.ogg"), text="Neeko on Nidalee")
-btn3.grid(row=0, column=2)
-
-btn4 = t.Button(win, command=lambda: play_quote("not neeko.ogg"), text="Not Neeko")
-btn4.grid(row=0, column=3)
-
-btn4 = t.Button(win, command=lambda: play_quote("not being neeko.ogg"), text="Not being Neeko")
-btn4.grid(row=0, column=4)
+#
+# champ_select = t.Combobox(win, values=champions)
+# champ_select.grid(row=0)
+# champ_select.bind("<<ComboboxSelected>>", lambda: get_btns(champions_selected[champ_select.current()]))
+# btn2 = t.Button(win, command=lambda: play_quote("Neeko_Select.ogg"), text="Neeko select")
+# btn2.grid(row=0, column=1)
+#
+# btn3 = t.Button(win, command=lambda: play_quote("Neeko_063.ogg"), text="Neeko on Nidalee")
+# btn3.grid(row=0, column=2)
+#
+# btn4 = t.Button(win, command=lambda: play_quote("not neeko.ogg"), text="Not Neeko")
+# btn4.grid(row=0, column=3)
+#
+# btn4 = t.Button(win, command=lambda: play_quote("not being neeko.ogg"), text="Not being Neeko")
+# btn4.grid(row=0, column=4)
 
 win.mainloop()
