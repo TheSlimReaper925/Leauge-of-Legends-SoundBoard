2727e99b4ff46ffe20e4ba4d047d8a985ad9256e
theexplosionmage <otar.phrodiashvili.1@btu.edu.ge>
2020-04-30 03:39:19 +0400

    Turned main page to proper class; Fixed button placement.

--- page_start.py
+++ page_start.py
@@ -3,82 +3,49 @@ import tkinter.ttk as ttk
 from PIL import Image, ImageTk
 
 
-import page_start_support
-
 def vp_start_gui():
-    '''Starting point when module is the main routine.'''
     global val, w, root
     root = tk.Tk()
     img = Image.open("materials/pic_page_start_background.jpg")
     photo = ImageTk.PhotoImage(img)
     top = Toplevel1 (photo, root)
-    page_start_support.init(root, top)
     root.mainloop()
-
-w = None
-def create_Toplevel1(rt, *args, **kwargs):
-    '''Starting point when module is imported by another module.
-       Correct form of call: 'create_Toplevel1(root, *args, **kwargs)' .'''
-    global w, w_win, root
-    #rt = root
-    root = rt
-    w = tk.Toplevel (root)
-    top = Toplevel1 (w)
-    page_start_support.init(w, top, *args, **kwargs)
-    return (w, top)
-
-def destroy_Toplevel1():
-    global w
-    w.destroy()
-    w = None
 
 class Toplevel1:
 
     def __init__(self, photo, top=None):
-        '''This class configures and populates the toplevel window.
-           top is the toplevel containing window.'''
 
         self.style = ttk.Style()
-        self.style.theme_use('winnative')
 
         top.geometry("800x580+652+102")
         top.resizable(0, 0)
         top.title("League Of Legends Sound Board")
 
-
         self.Label = ttk.Label(top, image=photo)
         self.Label.place(x=0, y=0, relwidth=1, relheight=1)
-
-        self.TButton1 = ttk.Button(top)
+        self.TButton1 = ttk.Button(top , command=None)
         self.TButton1.place(relx=0.5, rely=0.3, anchor=tk.CENTER, height=50, width=200, bordermode='ignore')
-        self.TButton1.configure(command=page_start_support.toChampionsPage)
-        self.TButton1.configure(takefocus="")
         self.TButton1.configure(text='''Champion List''')
-
-
 
 
         self.TButton1_1 = ttk.Button(top)
         self.TButton1_1.place(relx=0.5, rely=0.4, anchor=tk.CENTER, height=50, width=200
                 , bordermode='ignore')
-        self.TButton1_1.configure(command=page_start_support.toGamePage)
-        self.TButton1_1.configure(takefocus="")
+        self.TButton1_1.configure(command=None)
         self.TButton1_1.configure(text='''Guessing Game''')
-
-
-
-
 
 
         self.TButton1_2 = ttk.Button(top)
         self.TButton1_2.place(relx=0.5, rely=0.5, anchor=tk.CENTER, height=50, width=200
                 , bordermode='ignore')
-        self.TButton1_2.configure(command=page_start_support.toSupportLink)
+        self.TButton1_2.configure(command=None)
         self.TButton1_2.configure(takefocus="")
         self.TButton1_2.configure(text='''Support Us''')
 
 
 
-
 if __name__ == '__main__':
     vp_start_gui()
+
+
+
--- test.py
+++ test.py
@@ -1,45 +1,57 @@
+import tkinter as tk
+import tkinter.ttk as ttk
 from pygame import mixer
-from tkinter import *
-from tkinter import ttk as t
-
-win = Tk()
-win.title("test app")
-win.geometry("800x580")
-win.resizable(False, False)
 
 champions = ("Neeko", "other")
-
 neeko_quotes = ("Neeko_Select.ogg", "Neeko_Ban.ogg", "Star Guardian spawn music.ogg", "Neeko was out.ogg",
-                "not being neeko.ogg", "not neeko.ogg", "mimic jinx.ogg", "jungle buff neeko.ogg",
-                "last laugh - neeko.ogg", "Neeko - answers now.ogg", "Neeko - changing bodies.ogg",
-                "Neeko - danger beautiful.ogg", "Neeko - emotions.ogg", "Neeko - everywhere.ogg",
-                "Neeko - oovy cat.ogg", "Neeko - oyster.ogg", "Neeko - sho'ma.ogg", "Neeko - words are tricky.ogg",
-                "neeko first - friends.ogg", "no more room.ogg")
+                    "not being neeko.ogg", "not neeko.ogg", "mimic jinx.ogg", "jungle buff neeko.ogg",
+                    "last laugh - neeko.ogg", "Neeko - answers now.ogg", "Neeko - changing bodies.ogg",
+                    "Neeko - danger beautiful.ogg", "Neeko - emotions.ogg", "Neeko - everywhere.ogg",
+                    "Neeko - oovy cat.ogg", "Neeko - oyster.ogg", "Neeko - sho'ma.ogg", "Neeko - words are tricky.ogg",
+                    "neeko first - friends.ogg", "no more room.ogg")
 
-champions_selected = (neeko_quotes)
+def vp_start_gui():
 
-mixer.init()
-
-
-def play_quote(quote):
-    mixer.music.load("neeko voices/"+quote)
-    mixer.music.play()
+    global val, w, root
+    root = tk.Tk()
+    top = Toplevel2 (champions, neeko_quotes, root)
+    root.mainloop()
 
 
-def get_btn(event):
-    if champ_select.current() == 0:
-        z = 1
-        x = 0
-        for i in neeko_quotes:
-            t.Button(win, command=lambda y=i: play_quote(y), text=i[0:-4]).grid(row=z, column=x)
-            x += 1
-            if x == 5:
+class Toplevel2:
+
+    def play_quote(quote):
+        mixer.init()
+        mixer.music.load("neeko voices/" + quote)
+        mixer.music.play()
+
+
+    def __init__(self, champions, neeko_quotes, top=None):
+
+        top.title("test app")
+        top.geometry("800x580")
+        top.resizable(False, False)
+
+        def get_btn(event):
+            if self.champ_select.current() == 0:
+                z = 1
                 x = 0
-                z += 1
+                for i in neeko_quotes:
+                    button = tk.Button(top,  command=lambda y=i: Toplevel2.play_quote(y), text=i[0:-4]).place(relx=0.2*x+0.0065, rely=0.25*z-0.16, height=55, width=150)
+
+                    x += 1
+                    if x == 5:
+                        x = 0
+                        z += 1
+
+        self.champ_select = ttk.Combobox(top, values=champions)
+        self.champ_select.place(relx=0.0065, rely=0.0065)
+        self.champ_select.bind("<<ComboboxSelected>>", get_btn)
+        self.champions = champions
+        self.neeko_quotes = neeko_quotes
 
 
-champ_select = t.Combobox(win, values=champions)
-champ_select.grid(row=0)
-champ_select.bind("<<ComboboxSelected>>", get_btn)
 
-win.mainloop()
+
+if __name__ == '__main__':
+    vp_start_gui()
